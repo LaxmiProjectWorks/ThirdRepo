5dce4b6eeeb8b08b395f26b00a29a5a2f741555b
LaxmiRCode <laxmii1819@gmail.com>
2026-01-09 18:21:51 +0530

    nothing

--- DBConnection.java
+++ DBConnection.java
@@ -1,6 +1,9 @@
 class DBConnection{
     public static void main(String[] args){
         System.out.println("git restore <filename> will only discard the changes when the file in the unstaged area");
-        System.out.println("If we want to discard the changes for the Staging Area file then we need to perform unstage before git restore <filename>");
+        System.out.println("If we want to discard the changes for the Staging Area file then we need to 
+        perform unstage before git restore <filename>");
     }
+
+
 }
