81ea5edd81f3638caa1bb98f2ad61e3761fd3a8b
LaxmiRCode <laxmii1819@gmail.com>
2026-01-09 11:49:38 +0530

    Made changes in DBConnection

--- DBConnection.java
+++ DBConnection.java
@@ -1,5 +1,5 @@
 class DBConnection{
     public static void main(String[] args){
-        System.out.println("My name is Laxmi");
+        System.out.println("git restore <filename> will only discard the changes when the file in the unstaged area");
     }
 }
